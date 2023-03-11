e2bcb8a5346b24b2493be9d9fd443733afd80acb
Donghwi Kim <ehdgnl0904@naver.com>
2023-03-11 20:46:38 +0900

    Refactor: file path refactoring

--- Chapter03. 기초 프로그램 만들기/4. QR코드 생성기/main4-1.py
+++ Chapter03. 기초 프로그램 만들기/4. QR코드 생성기/main4-1.py
@@ -3,5 +3,5 @@ import qrcode
 qr_data = 'www.naver.com'
 qr_img = qrcode.make(qr_data)
 
-save_path = r'C:\Users\ehdgn\PycharmProjects\파이썬과 40개의 작품들\Chapter03. 기초 프로그램 만들기\4. QR코드 생성기\\' + qr_data + '.png'
+save_path = qr_data + '.png'
 qr_img.save(save_path)
