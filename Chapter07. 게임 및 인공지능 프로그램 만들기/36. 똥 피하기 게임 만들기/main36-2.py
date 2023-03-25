1ad1e701f40d7a7329874c4558ab3d31ede87fb7
Donghwi Kim <ehdgnl0904@naver.com>
2023-03-25 23:35:49 +0900

    Create a game avoid poop

---
+++ Chapter07. 게임 및 인공지능 프로그램 만들기/36. 똥 피하기 게임 만들기/main36-2.py
@@ -0,0 +1,51 @@
+import pygame, sys
+from pygame.locals import *
+import random
+
+FPS = 60
+MAX_WIDTH = 400
+MAX_HEIGHT = 600
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((MAX_WIDTH,MAX_HEIGHT))
+
+
+class Player():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        return pygame.draw.rect(screen, (0,0,255), (self.x, self.y, 40, 40))
+
+    # 키보드의 좌우 화살표 키를 입력받아 좌우로 이동하게 한다. 화면 밖으로 나가지 못하도록 한다.
+    def move(self):
+        if pressed_keys[K_RIGHT]:
+            if self.x < MAX_WIDTH-40:
+                self.x += 5
+        if pressed_keys[K_LEFT]:
+            if self.x > 0:
+                self.x -= 5
+
+
+player = Player(MAX_WIDTH/2, MAX_HEIGHT - 40)
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                    
+        clock.tick(FPS) 
+        screen.fill((255, 255, 255))
+        global pressed_keys
+        pressed_keys = pygame.key.get_pressed()
+        player_rect = player.draw()
+        player.move()
+        
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()
